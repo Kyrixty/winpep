484817f74e57f352ba27b8abf87373ffca606c81
shirtydiamond33 <kyrixty@gmail.com>
2026-05-15 21:51:14 -0400

    add next goal

--- src/main.c
+++ src/main.c
@@ -5,6 +5,6 @@
 int main() {
     arena_t* arena = arena_init(MB(64));
     coff_t coff = load_coff("./build/sample.o", arena);
-
+    /* Next goals are to understand storage classes (at least SMBL_EXT, SMBL_STAT, SMBL_FILE)*/
     return 0;
 }
