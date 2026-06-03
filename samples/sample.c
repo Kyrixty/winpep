2383ec33a56b3c08c4b8a0727fdf9b317913f9c6
shirtydiamond33 <kyrixty@gmail.com>
2026-06-03 01:32:12 -0400

    add xtra documentation in sample

--- samples/sample.c
+++ samples/sample.c
@@ -7,21 +7,38 @@ int externallyAccessible = 40;
 extern int externallyDefined;
 /* Unless you specify the pointer as const
 (i.e. T* const var) then only the data will be
-considered const (and therefore held in rdata)*/
+considered const (and therefore held in rdata.)
+
+Essentially, a string literal will always be in .rdata.
+However, the variable aliasing it may not (dataStr, alsoDataStr)
+UNLESS that variable is also marked const AND is global.*/
 static const char* dataStr = "Hello, rdata (from data)!";
 static const char const* alsoDataStr = "Hello, rdata (from data)2!";
 static const char* const rdataStr  = "Hello, rdata (from rdata)!";
 
 void my_func(int x) {
+    /**
+     * Any static variable will be put in .data
+     * since it must remember it's value from the previous call.
+     * However, you cannot access it outside of my_func (without
+     * storing a pointer to it) and thus it is sort of a scoped
+     * static variable.
+     */
     static int staticInNonStatic = 12;
     int nonStaticInsideNonStatic = 13;
-    printf("x is %d\n", x);
+    /**
+     * Because constNotInData is scoped to my_func's lifetime, it does
+     * not need to exist for the entire lifetime of the program and thus
+     * is not in .data (in fact, there is no symbol for constNotInData.)
+     */
+    char* const constNotInData = "This literal is also in .rdata despite not being marked as const";
+    printf("x is %d\n", x); // this string has the same address in .rdata as @x
 }
 
 static void my_func_static(int x) {
     static int staticInsideStatic = 14;
     int nonStaticInsideStatic = 15;
-    printf("x is %d\n", x);
+    printf("x is %d\n", x); // @x
     printf("externallyDefined is %d\n", externallyDefined);
 }
 
